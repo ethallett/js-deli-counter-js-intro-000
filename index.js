8e7b1b715dd05a055b4a9841d3ad95ae37b1efc8
Elizabeth T Hallett <ethallett@gmail.com>
2020-01-09 02:17:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function takeANumber(katzDeliLine, name){
 
 function nowServing(katzDeliLine){
   if (katzDeliLine.length >= 1){
-    return katzDeliLine.shift()
+    return `Currently serving ${katzDeliLine.shift()}`
   } else {
     return "There is nobody waiting to be served!"
   }
