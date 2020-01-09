2a9ea509d6462249ffffa5674610b7a758b4720b
Elizabeth T Hallett <ethallett@gmail.com>
2020-01-09 03:05:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function currentLine(katzDeliLine){
   return 'The line is currently empty.'
 }
   for (let n = 0; n < katzDeliLine.length; n++) {
-    array.push(`${n+1}. ${katzDeliLine[n]}`)
+    array.push(`${n+1}. ${katzDeliLine[n]},`)
   }
   return line += array.join(" ")
 }
