ee0a0c8572ffb8932623ebb1db40a54cc781e6a7
Elizabeth T Hallett <ethallett@gmail.com>
2020-01-09 02:07:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var katzDeliLine = []
 
 function takeANumber(katzDeliLine, name){
+  katzDeliLine.push(name)
   if (katzDeliLine.length >= 1) {
     return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
  }
